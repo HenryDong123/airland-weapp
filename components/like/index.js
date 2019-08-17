8a7df66f3edeba05b35045ffceb13d4d1cf8b00a
HenryDong <943082205@qq.com>
2019-08-17 20:11:54 +0800

    简单完成like组件逻辑

--- components/like/index.js
+++ components/like/index.js
@@ -1,30 +1,37 @@
 // components/like/index.js
 Component({
-	/**
-	 * 组件的属性列表
-	 */
-	properties: {
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        like: {
+            type: Boolean
+        },
+        count: {
+            type: Number
+        }
+    },
 
-	},
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        likeImg: '../image/like.png',
+        dislikeImg: '../image/like@dis.png'
+    },
 
-	/**
-	 * 组件的初始数据
-	 */
-	data: {
-		like: false,
-		count: 9,
-		test: 99
-	},
-
-	/**
-	 * 组件的方法列表
-	 */
-	methods: {
-		onLike(e) {
-			console.log()
-			this.setData({
-				like: !this.data.like
-			})
-		}
-	}
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onLike(e) {
+            let like = this.properties.like
+            let count = this.properties.count
+			count = like?count-1:count+1
+            this.setData({
+                like: !like,
+                count
+            })
+        }
+    }
 })
