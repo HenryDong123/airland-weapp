8f32ab7bd296f261231f7036952a6d37f6fb76e4
HenryDong <943082205@qq.com>
2019-08-22 21:00:07 +0800

    添加音乐期刊的播放暂停，待完善切换逻辑

--- components/classic/music/index.js
+++ components/classic/music/index.js
@@ -1,25 +1,44 @@
 // components/classic/music/index.js
 import {classicBeh} from "../classic-beh";
 
+const mMgr = wx.getBackgroundAudioManager()
 Component({
-	/**
-	 * 组件的属性列表
-	 */
-	behaviors: [classicBeh],
+    /**
+     * 组件的属性列表
+     */
+    behaviors: [classicBeh],
+    properties: {
+        musicSrc: String,
+        title: String
+    },
 
-	/**
-	 * 组件的初始数据
-	 */
-	data: {
-			playing: false,
-			pauseSrc: '../../image/player@pause.png',
-			playSrc: '../../image/player@play.png',
-	},
+    /**
+     * 组件的初始数据
+     */
+    data: {
+        playing: false,
+        pauseSrc: '../../image/player@pause.png',
+        playSrc: '../../image/player@play.png',
+    },
 
-	/**
-	 * 组件的方法列表
-	 */
-	methods: {
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        onPlay() {
+            if (this.data.playing) {
+                this.setData({
+                    playing: false
+                })
+                mMgr.pause()
+            } else {
+                this.setData({
+                    playing: true
+                })
+                mMgr.title = this.properties.title
+                mMgr.src = this.properties.musicSrc
+            }
 
-	}
+        }
+    }
 })
